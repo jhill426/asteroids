4b0f12dcdd5c37865eb7f1c07a809ffb413e4128
Justin Hill <jhill@figma.com>
2025-08-09 17:04:44 -0400

    Added constants for the project

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import pygame
+from constants import *
+
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"ASTEROID_MIN_RADIUS: {ASTEROID_MIN_RADIUS}")
+    print(f"ASTEROID_KINDS: {ASTEROID_KINDS}")
+    print(f"ASTEROID_SPAWN_RATE: {ASTEROID_SPAWN_RATE}")
+    print(f"ASTEROID_MAX_RADIUS: {ASTEROID_MAX_RADIUS}")
 
 
 if __name__ == "__main__":
