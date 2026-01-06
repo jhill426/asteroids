89fb419d5d08b8b4d76741700f91bd1afb89e1cb
Justin Hill <jhill@figma.com>
2026-01-06 15:51:52 -0800

    Added some event logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logger import log_state, log_event
 import pygame
 import sys
 from constants import *
@@ -5,6 +6,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
+
 
 def main():
     pygame.init()
@@ -15,17 +17,21 @@ def main():
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
-    Player.containers = (updatable, drawable) 
+    Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = updatable
     Shot.containers = (shots, updatable, drawable)
-    
+
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     asteroid_field = AsteroidField()
 
+    print("Starting Asteroids")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     running = True
     while running:
         dt = clock.tick(60) / 1000.0
+        log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -37,7 +43,7 @@ def main():
             if player.collide(asteroid):
                 print("Game over!")
                 sys.exit()
-        
+
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collide(shot):
@@ -50,6 +56,7 @@ def main():
         pygame.display.flip()
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
